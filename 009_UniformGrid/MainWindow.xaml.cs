dee1a41b764c4bff64d1a68b4f9049e4a447e48d
js1266666666 <127263671+js1266666666@users.noreply.github.com>
2023-05-03 10:55:33 +0900

    Add files via upload

---
+++ 009_UniformGrid/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _009_UniformGrid
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChessBoard.Rows = 8;
+            ChessBoard.Columns = 8;
+
+            for(int i=0; i<64; i++)
+            {
+                if((i/4)%2 == 0)
+                {
+                    Rectangle r1 = new Rectangle();
+                    r1.Fill = Brushes.Black;
+                    ChessBoard.Children.Add(r1);
+
+                    Rectangle r2 = new Rectangle();
+                    r2.Fill = Brushes.Red;
+                    ChessBoard.Children.Add(r2);
+                }
+                else
+                {
+                    Rectangle r1 = new Rectangle();
+                    r1.Fill = Brushes.Red;
+                    ChessBoard.Children.Add(r1);
+
+                    Rectangle r2 = new Rectangle();
+                    r2.Fill = Brushes.Black;
+                    ChessBoard.Children.Add(r2);
+                }
+            }
+        }
+    }
+}

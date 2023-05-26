8d186b5bcd96573c6fce96b9b74ecc855f54c9c5
js1266666666 <127263671+js1266666666@users.noreply.github.com>
2023-05-26 15:48:42 +0900

    Add files via upload

---
+++ 012_TwoWindows/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _012_TwoWindows
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.Show();//Show => modaless로 보여준다.
+        }
+
+        private void btnDialog_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.ShowDialog();//ShowDialog => dialog로 보여준다.(다른 창 클릭이 안됨)
+        }
+    }
+}
